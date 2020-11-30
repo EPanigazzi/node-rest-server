974faee0b1245fe1f8ef365c5bb9af2d2c16c16c
Ezequiel Panigazzi <50381451+EPanigazzi@users.noreply.github.com>
2020-11-30 13:27:53 -0300

    Variable de entorno MONGO_URI

--- server/config/config.js
+++ server/config/config.js
@@ -8,17 +8,17 @@ process.env.PORT = process.env.PORT || 3000;
 // ===================
 // ENTORNO
 //====================
-process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+process.env.NODE_ENV = process.env.NODE_ENV || "dev";
 
 // ===================
 // BASE DE DATOS
 //====================
 let urlDB;
 
-if(process.env.NODE_ENV === 'dev'){
-    urlDB = 'mongodb://localhost:27017/cafe'
-}else{
-    urlDB = 'mongodb+srv://pani-admin:y0zTveG0ryVJF010@pani-pruebas.gmweq.mongodb.net/test'
+if (process.env.NODE_ENV === "dev") {
+    urlDB = "mongodb://localhost:27017/cafe";
+} else {
+    urlDB = process.env.MONGO_URI;
 }
 // enviroment inventado para usar en server.js
-process.env.URLDB = urlDB
+process.env.URLDB = urlDB;
